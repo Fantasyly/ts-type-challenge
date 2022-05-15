9e20e2865e0b27be6d78dc8e7ad7f03ca44c28af
望切 <wangqie@WangdeAir.mshome.net>
2022-05-15 22:13:28 +0800

    add: 完善04

--- src/04-easy-pick/template.ts
+++ src/04-easy-pick/template.ts
@@ -13,3 +13,29 @@ type MyPick <T, K extends keyof T> = {
  *       因此应该去extends T的key值的集合
  *       所以 应该是 K extends keyof T
  */
+
+// js
+// 对比学习法
+function myPick(todo, keys) {
+  const obj = {};
+
+  keys.forEach((key) => {
+    if (key in todo) {
+      obj[key] = todo[key];
+    }
+  });
+
+  return obj;
+}
+
+// 涉及到的知识点
+// 1. 返回一个对象
+// 2. 遍历foreach  mapped
+// - https://www.typescriptlang.org/docs/handbook/2/mapped-types.html
+// 3. todo[key] 取值 indexed
+// - https://www.typescriptlang.org/docs/handbook/2/indexed-access-types.html
+// 4. 看看 key 在不在 todo 里面
+//    1. keyof  lookup
+//     - https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-1.html#keyof-and-lookup-types
+//    2. extends 条件约束 
+//     - https://www.typescriptlang.org/docs/handbook/2/generics.html#generic-constraints
