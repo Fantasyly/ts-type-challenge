0c2ce354a5f4587d3b9ae9051de3dc26d5acdbdb
望切 <wangqie@WangdeMacBook-Air.local>
2022-06-04 10:16:49 +0800

    feat: 复习11题

---
+++ src/easy/11-easy-tuple-to-object/review1.ts
@@ -0,0 +1,10 @@
+/**
+ * 传入一个元组类型，将这个元组类型转换为对象类型，这个对象类型的键/值都是从元组中遍历出来。
+ */
+export type TupleToObject<T extends readonly (string | number | symbol)[]> = {
+  [U in T[number]]: U;
+};
+
+const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const;
+// expected { tesla: 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
+type result = TupleToObject<typeof tuple>;
