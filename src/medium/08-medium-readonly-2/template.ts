374f3b84fff3bb684dc372879e8f9a565a10d2dc
望切 <wangqie@WangdeMacBook-Air.local>
2022-06-14 11:58:26 +0800

    feat: 完善中等08

--- src/medium/08-medium-readonly-2/template.ts
+++ src/medium/08-medium-readonly-2/template.ts
@@ -3,10 +3,16 @@
  * `K`指定应设置为Readonly的`T`的属性集。
  * 如果未提供`K`，则应使所有属性都变为只读，就像普通的`Readonly<T>`一样。
  */
-export type MyReadonly2<T, K extends keyof T = keyof T> = {
-  readonly [S in K]: T[S];
+type MyReadonly2<T, K extends keyof T = keyof T> = {
+  readonly [S in K]: T[S]; // 这一步其实相当于方法2中的Readonly<Pick<>>
 } & Omit<T, K>
+
+// 解法2: 先在T中Pick出K，然后加上Readonly，随后和Omit的结果&
+// export type MyReadonly2<T, K extends keyof T = keyof T> = Readonly<Pick<T, K>> &
+//   Omit<T, K>;
+
 /**
+ * 涉及到的知识点
  * 1: 给范型赋默认值
  */
 
